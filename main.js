c0b295dbb8f7a8deae76fc980395340078b0009a
Albab05 <albabkabir05@gmail.com>
2021-07-30 21:50:29 +0530

    Adding crops and number system

--- main.js
+++ main.js
@@ -172,6 +172,96 @@ function submit()
                 if(land >0){f_output.innerHTML = `Use NPK fertilizer of ${land*90/2.5} kg N(Nitrogen), ${land*90/2.5} kg P(Phosphorus), ${land*90/2.5} kg K(Potassium)`;}
                 else{f_output.innerHTML = ""}
                 break;
+                case 'tomato':
+                if(ph<6 && ph>=0 && land>0){ph_output.innerHTML = `Apply ${land*1200*(6-ph)} kg of lime`;}
+                else if(ph>7 && land>0){ph_output.innerHTML = `Apply ${land*1200*(ph-7)} kg of sulphur`;}
+                else if(ph<=7 && ph>=6){ph_output.innerHTML = "Ideal pH";}
+                else{ph_output.innerHTML = "";}
+                if(sar>6){sar_output.innerHTML = `Apply ${land*1700*(sar-6)} kg of gypsum`;}
+                else if(sar<=6 && sar>=0 && land>0){sar_output.innerHTML = "Ideal Sodium Adsorption Ratio";}
+                else{sar_output.innerHTML = "";}
+                if(ec<2.5 && ec>=0){ec_output.innerHTML = `Use saline water for irrigation`;}
+                else if(ec>2.5){ec_output.innerHTML = `Use fresh water for irrigation`;}
+                else if(ec==2.5){ec_output.innerHTML = "Ideal Electrical Conductivity";}
+                else{ec_output.innerHTML = "";}
+                if(cec<10 && cec>=0){cec_output.innerHTML = `Use organic matter`;}
+                else if(cec>10){cec_output.innerHTML = "Ideal Cation Exchange Capacity";}
+                else{cec_output.innerHTML = "";}
+                if(land > 0){f_output.innerHTML = `Use ${374/2.5*land} kg of Ammonium Nitrate(34-0-0), ${119/2.5*land} of Calcium Nitrate(26% CaO), ${112.5/2.5*land} kg of Magnesium Sulphate(16% MgO), ${1080.4/2.5*land} kg of Multi-K(13-0-46), ${139/2.5*land} kg of Haifa-MAP(12-61-0)`;}
+                else{f_output.innerHTML = "";}
+                break;
+                case 'onion':
+                if(ph<6 && ph>=0 && land>0){ph_output.innerHTML = `Apply ${land*1200*(6-ph)} kg of lime`;}
+                else if(ph>7 && land>0){ph_output.innerHTML = `Apply ${land*1200*(ph-7)} kg of sulphur`;}
+                else if(ph<=7 && ph>=6){ph_output.innerHTML = "Ideal pH";}
+                else{ph_output.innerHTML = "";}
+                if(sar>6){sar_output.innerHTML = `Apply ${land*1700*(sar-6)} kg of gypsum`;}
+                else if(sar<=6 && sar>=0 && land>0){sar_output.innerHTML = "Ideal Sodium Adsorption Ratio";}
+                else{sar_output.innerHTML = "";}
+                if(ec<1.4 && ec>=0){ec_output.innerHTML = `Use saline water for irrigation`;}
+                else if(ec>1.4){ec_output.innerHTML = `Use fresh water for irrigation`;}
+                else if(ec==1.4){ec_output.innerHTML = "Ideal Electrical Conductivity";}
+                else{ec_output.innerHTML = "";}
+                if(cec<10 && cec>=0){cec_output.innerHTML = `Use organic matter`;}
+                else if(cec>10){cec_output.innerHTML = "Ideal Cation Exchange Capacity";}
+                else{cec_output.innerHTML = "";}
+                if(land >0){f_output.innerHTML = `Use NPK fertilizer of ${land*40/2.5} kg N(Nitrogen), ${land*40/2.5} kg P(Phosphorus), ${land*60/2.5} kg K(Potassium)`;}
+                else{f_output.innerHTML = "";}
+                break;
+                case 'corn':
+                if(ph<5.8 && ph>=0 && land>0){ph_output.innerHTML = `Apply ${land*1200*(6-ph)} kg of lime`;}
+                else if(ph>7 && land>0){ph_output.innerHTML = `Apply ${land*1200*(ph-7)} kg of sulphur`;}
+                else if(ph<=7 && ph>=5.8){ph_output.innerHTML = "Ideal pH";}
+                else{ph_output.innerHTML = "";}
+                if(sar>6){sar_output.innerHTML = `Apply ${land*1700*(sar-6)} kg of gypsum`;}
+                else if(sar<=6 && sar>=0 && land>0){sar_output.innerHTML = "Ideal Sodium Adsorption Ratio";}
+                else{sar_output.innerHTML = "";}
+                if(ec<1.7 && ec>=0){ec_output.innerHTML = `Use saline water for irrigation`;}
+                else if(ec>1.7){ec_output.innerHTML = `Use fresh water for irrigation`;}
+                else if(ec==1.7){ec_output.innerHTML = "Ideal Electrical Conductivity";}
+                else{ec_output.innerHTML = "";}
+                if(cec<10 && cec>=0){cec_output.innerHTML = `Use organic matter`;}
+                else if(cec>10){cec_output.innerHTML = "Ideal Cation Exchange Capacity";}
+                else{cec_output.innerHTML = "";}
+                if(land >0){f_output.innerHTML = `Use NPK fertilizer of ${land*135/2.5} kg N(Nitrogen), ${land*62/2.5} kg P(Phosphorus), ${land*50/2.5} kg K(Potassium) , ${land*37.5/2.5} kh of ZnSO4`;}
+                else{f_output.innerHTML = "";}
+                break;
+                case 'soybean':
+                if(ph<5.2 && ph>=0 && land>0){ph_output.innerHTML = `Apply ${land*1200*(6-ph)} kg of lime`;}
+                else if(ph>6.5 && land>0){ph_output.innerHTML = `Apply ${land*1200*(ph-7)} kg of sulphur`;}
+                else if(ph<=6.5 && ph>=5.2){ph_output.innerHTML = "Ideal pH";}
+                else{ph_output.innerHTML = "";}
+                if(sar>6){sar_output.innerHTML = `Apply ${land*1700*(sar-6)} kg of gypsum`;}
+                else if(sar<=6 && sar>=0 && land>0){sar_output.innerHTML = "Ideal Sodium Adsorption Ratio";}
+                else{sar_output.innerHTML = "";}
+                if(ec<5 && ec>=0){ec_output.innerHTML = `Use saline water for irrigation`;}
+                else if(ec>5){ec_output.innerHTML = `Use fresh water for irrigation`;}
+                else if(ec==5){ec_output.innerHTML = "Ideal Electrical Conductivity";}
+                else{ec_output.innerHTML = "";}
+                if(cec<10 && cec>=0){cec_output.innerHTML = `Use organic matter`;}
+                else if(cec>10){cec_output.innerHTML = "Ideal Cation Exchange Capacity";}
+                else{cec_output.innerHTML = "";}
+                if(land >0){f_output.innerHTML = `Use NPKS fertilizer of ${land*20/2.5} kg N(Nitrogen), ${land*40/2.5} kg P(Phosphorus), ${land*20/2.5} kg K(Potassium), ${land*20/2.5} kh of S(Sulphur)`;}
+                else{f_output.innerHTML = "";}
+                break;
+                case 'orange':
+                if(ph<6 && ph>=0 && land>0){ph_output.innerHTML = `Apply ${land*1200*(6-ph)} kg of lime`;}
+                else if(ph>7.6 && land>0){ph_output.innerHTML = `Apply ${land*1200*(ph-7)} kg of sulphur`;}
+                else if(ph<=7.5 && ph>=6){ph_output.innerHTML = "Ideal pH";}
+                else{ph_output.innerHTML = "";}
+                if(sar>6){sar_output.innerHTML = `Apply ${land*1700*(sar-6)} kg of gypsum`;}
+                else if(sar<=6 && sar>=0 && land>0){sar_output.innerHTML = "Ideal Sodium Adsorption Ratio";}
+                else{sar_output.innerHTML = "";}
+                if(ec<1.7 && ec>=0){ec_output.innerHTML = `Use saline water for irrigation`;}
+                else if(ec>1.7){ec_output.innerHTML = `Use fresh water for irrigation`;}
+                else if(ec==1.7){ec_output.innerHTML = "Ideal Electrical Conductivity";}
+                else{ec_output.innerHTML = "";}
+                if(cec<10 && cec>=0){cec_output.innerHTML = `Use organic matter`;}
+                else if(cec>10){cec_output.innerHTML = "Ideal Cation Exchange Capacity";}
+                else{cec_output.innerHTML = "";}
+                if(land >0){f_output.innerHTML = `Use NPK fertilizer of ${land*140/2.5} kg N(Nitrogen), ${land*40/2.5} kg P(Phosphorus), ${land*100/2.5} kg K(Potassium)`;}
+                else{f_output.innerHTML = "";}
+                break;
         default:
             break;
     }
